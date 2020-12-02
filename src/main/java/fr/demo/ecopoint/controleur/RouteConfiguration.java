3aebae96f793e21291fb69bd2a1dd6268d92f4b6
darkunz34 <stephan.parichon.pro@gmail.com>
2020-12-02 22:38:47 +0100

    Ajout des pages html avec la récupération de données issue du role de l'utilisateur. 3 rôles déterminer : - ADMIN => Nous - USER => client - ROLE_ANONYMOUS => client non connecter

---
+++ src/main/java/fr/demo/ecopoint/controleur/RouteConfiguration.java
@@ -0,0 +1,27 @@
+package fr.demo.ecopoint.controleur;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Ensemble des routes pouvant êre pointer sur l'application.
+ */
+@Configuration
+public class RouteConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //page index accessible en client co et non co
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/test").setViewName("test");
+
+        //page de connexion
+        registry.addViewController("/login").setViewName("login");
+
+        //page pour administrateur
+        registry.addViewController("/admin/connecter").setViewName("admin/connecter");
+        registry.addViewController("/admin/admin").setViewName("admin/admin");
+    }
+}
